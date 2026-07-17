dc2a0b4b1f377f405596a72af9a6742d6220feca
LoveGraphics <duypham2008graphics@outlook.com>
2026-07-17 19:31:51 +0700

    Update plugin.py

--- src/endstone_essentialstone/plugin.py
+++ src/endstone_essentialstone/plugin.py
@@ -1,6 +1,7 @@
+import json
+
 from endstone.plugin import Plugin
 from typing_extensions import override
-
 
 class EssentialStone(Plugin):
    prefix = "EssentialStone"
@@ -9,7 +10,17 @@ class EssentialStone(Plugin):
 
    @override
    def on_load(self) -> None:
-      self.logger.info("Initalizing datas...")
+      self.logger.info("Load languages...")
+      self.save_resources("eng.json")
+      language_data_path = self.data_folder / (self.config.get("language") + ".json")
+      try:
+          with open(language_data_path, "r") as f:
+              language = json.load(f)
+              self.logger.info("Current language: " + self.config.get("language"))
+      except:
+          self.logger.info("Invalid language in config file, disabling...")
+          self.server.plugin_manager.disable_plugin(self)
+
 
    @override
    def on_enable(self) -> None:
