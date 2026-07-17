85e4702f7e57ad86f1bd9937a710fd292e54bd51
LoveGraphics <duypham2008graphics@outlook.com>
2026-07-17 22:30:55 +0700

    rewrite config and language

--- src/endstone_essentialstone/plugin.py
+++ src/endstone_essentialstone/plugin.py
@@ -1,21 +1,12 @@
-import json
-
-from endstone.plugin import Plugin, PluginCommand
+from endstone.plugin import Plugin
 from typing_extensions import override
-
-<<<<<<< HEAD
-from endstone_essentialstone.Command import *
-=======
-from endstone_essentialstone.utils.config import Config
-
->>>>>>> e6c91a9fb3c486f764d628af3c06d971fade0d63
+from endstone_essentialstone.utils.language import Language
 
 class EssentialStone(Plugin):
     prefix = "EssentialStone"
 
     api_version = "0.11"
 
-<<<<<<< HEAD
     commands = {
         "tpa": {
             "description": "",
@@ -32,19 +23,10 @@ class EssentialStone(Plugin):
     @override
     def on_load(self) -> None:
         self.logger.info("Initalizing datas...")
-        self.pluginConfig = Config(self)
-
+        
+        self.save_default_config()
+        self.language = Language(self, self.config["language"]["lang"])
 
     @override
     def on_enable(self) -> None:
         pass
-=======
-   @override
-   def on_load(self) -> None:
-      self.logger.info("Initalizing datas...")
-      self.pluginConfig = Config(self)
-      
-   @override
-   def on_enable(self) -> None:
-       pass
->>>>>>> e6c91a9fb3c486f764d628af3c06d971fade0d63
--- src/endstone_essentialstone/utils/language.py
+++ src/endstone_essentialstone/utils/language.py
@@ -1,13 +1,25 @@
+import json
+
 from endstone.plugin import Plugin
 
-from endstone_essentialstone.utils.config import Config
-
-
 class Language:
-    def __init__(self, plugin: Plugin, base_language):
-        self.plugin = plugin
-        self.base_language = base_language
-        self.config = Config(plugin)
 
-    def is_available(self):
-        return self.config.get_configuration_file("languages/" + self.base_language)
+    def __init__(self, plugin: Plugin, language: str):
+        self.plugin = plugin
+        self.language = language
+
+        resources_path = self.plugin.data_folder / "resources"
+        if not resources_path.exists(): resources_path.mkdir()
+        language_path = self.plugin.data_folder / "resources" / "languages"
+        if not language_path.exists(): language_path.mkdir()
+
+        self.LoadLanguageData()
+
+    def LoadLanguageData(self) -> bool:
+        self.plugin.save_resources("resources/languages/" + self.language + ".json")
+
+        language_data_path = self.plugin.data_folder / "resources" / "languages" / (self.language + ".json")
+        try:
+            with open(language_data_path, "r") as f: self.data = json.load(f)
+        except:
+            self.plugin.plugin_loader.disable_plugin(self.plugin)
