72567c38a5eb1d286eed81b5cf0219a3bda37f1f
LoveGraphics <duypham2008graphics@outlook.com>
2026-07-18 00:14:13 +0700

    update tpa

--- src/endstone_essentialstone/plugin.py
+++ src/endstone_essentialstone/plugin.py
@@ -1,3 +1,7 @@
+import time
+import uuid
+
+from endstone.command import Command, CommandSender
 from endstone.plugin import Plugin
 from typing_extensions import override
 from endstone_essentialstone.utils.language import Language
@@ -10,12 +14,20 @@ class EssentialStone(Plugin):
     commands = {
         "tpa": {
             "description": "",
-            "usages": ["/tpa"],
+            "usages": ["/tpa [target: player]"],
             "permissions": ["essentialstone.command.tpa"]
+        },
+        "tpaccept": {
+            "description": "",
+            "usages": ["/tpaccept [sender: player]"],
+            "permissions": ["essentialstone.command.tpaccept"]
         }
     }
     permissions = {
         "essentialstone.command.tpa": {
+            "default": True
+        },
+        "essentialstone.command.tpaccept": {
             "default": True
         }
     }
@@ -27,6 +39,67 @@ class EssentialStone(Plugin):
         self.save_default_config()
         self.language = Language(self, self.config["language"]["lang"])
 
+        # Command's data
+        self.tpaSettings = {}
+        self.tpaCooldown = {}
+        self.tpaRequest = {}
+
     @override
     def on_enable(self) -> None:
-        pass
+        self.server.scheduler.run_task(self, self.TpaCooldownCleaner, 0, self.tpaSettings["request-cooldown"] * 20)
+        self.server.scheduler.run_task(self, self.TpaRequestCleaner, 0, self.tpaSettings["request-expired-time"] * 20)
+
+    def on_command(self, sender: CommandSender, command: Command, args: list[str]) -> bool:
+        match command.name:
+            case "tpa":
+                if args[0] == sender.name:
+                    sender.send_message(self.language.data["tpa"]["tpa-request-self"])
+                    return False
+                currentTime = time.time()
+                requestCooldown = self.tpaSettings["request-cooldown"]
+                senderUUID = str(self.server.get_player(sender.name).unique_id)
+                if not self.tpaCooldown[senderUUID]: self.tpaCooldown[senderUUID] = currentTime - requestCooldown
+                if currentTime - self.tpaCooldown[senderUUID] < requestCooldown:
+                    sender.send_message(self.language.data["tpa"]["tpa-cooldown"].replace("{cooldown}", str(requestCooldown)))
+                    return False
+                targetUUID = str(self.server.get_player(args[0]).unique_id)
+                self.tpaCooldown[senderUUID] = currentTime
+                if not self.tpaRequest[targetUUID]: self.tpaRequest[targetUUID] = {}
+                self.tpaRequest[targetUUID][senderUUID] = currentTime
+                sender.send_message(self.language.data["tpa"]["tpa-request-sent"].replace("{target_name}", args[0]))
+                self.server.get_player(args[0]).send_message(self.language.data["tpa"]["tpa-request-received"].replace("{sender_name}", sender.name))
+                
+                return True
+            
+            case "tpaccept":
+                if len(args) == 0:
+                    senderUUID = str(self.server.get_player(sender.name).unique_id)
+                    if not self.tpaRequest.get(senderUUID):
+                        sender.send_message(self.language.data["tpa"]["tpa-request-empty"])
+                        return False
+                    if len(self.tpaRequest[senderUUID]) == 0:
+                        sender.send_message(self.language.data["tpa"]["tpa-request-empty"])
+                        return False
+                    senderCUUID = next(iter(self.tpaRequest[senderUUID].popitem()))
+                    if len(self.tpaRequest[senderUUID]) == 0: self.tpaRequest.pop(senderUUID)
+                    senderCPlayer = self.server.get_player(uuid.UUID(senderCUUID))
+                    senderCPlayer.teleport(self.server.get_player(sender.name))
+                    sender.send_message(self.language.data["tpa"]["tpa-request-accepted_receiver"])
+                    senderCPlayer.send_message(self.language.data["tpa"]["tpa-request-accepted_sender"])
+
+        return True
+    
+    def TpaCooldownCleaner(self) -> None:
+        currentTime = time.time()
+        requestCooldown = self.tpaSettings["request-cooldown"]
+        for uuid, cooldown in self.tpaCooldown.items():
+            if currentTime - cooldown >= requestCooldown: self.tpaCooldown.pop(uuid)
+
+    def TpaRequestCleaner(self) -> None:
+        currentTime = time.time()
+        expiredTime = self.tpaSettings["request-expired-time"]
+        for targetUUID, requests in self.tpaRequest.items():
+            for senderUUID, expiredTime_ in requests.items():
+                if currentTime - expiredTime_ >= expiredTime:
+                    requests.pop(senderUUID)
+                    if len(requests) == 0: self.tpaRequest.pop(targetUUID)
